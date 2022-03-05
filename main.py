d7539c24ff4c62796260c70fc26b6735dd3f2df8
st10-01-16 <artu.200505@gmail.com>
2022-03-05 15:25:16 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from data import db_session
+from data.db_session import create_session
+from data.users import User
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    session = create_session()
+
+    # user = User()
+    # user.name = "Пользователь 1"
+    # user.about = "биография пользователя 1"
+    # user.email = "email@email.ru"
+    # db_sess = db_session.create_session()
+    # db_sess.add(user)
+    # db_sess.commit()
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
