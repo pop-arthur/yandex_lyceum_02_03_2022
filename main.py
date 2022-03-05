f8fcdc0b42215f14a5ed0dcc0ea6739fdcc245b1
st10-01-16 <artu.200505@gmail.com>
2022-03-05 16:22:12 +0500

    task 4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from flask import Flask
 from data.db_session import global_init, create_session
 from data.users import User
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -10,36 +12,14 @@ def main():
     global_init("db/mars_explorer.db")
     session = create_session()
 
-    user = User()
-    user.surname = "Бледный"
-    user.name = "Кирилл"
-    user.age = 24
-    user.position = "местный дед инсайд"
-    user.speciality = "солист группы Пошлая Моль"
-    user.address = "Москва"
-    user.email = "kirill777@mail.ru"
-    session.add(user)
-
-
-    user = User()
-    user.surname = "Del Ray"
-    user.name = "Lana"
-    user.age = 35
-    user.position = "cooker"
-    user.speciality = "singer"
-    user.address = "America"
-    user.email = "lanochka@gmail.com"
-    session.add(user)
-
-    user = User()
-    user.surname = "Popov"
-    user.name = "Arthur"
-    user.age = 17
-    user.position = "DJ"
-    user.speciality = "perfect music player"
-    user.address = "101 alfa"
-    user.email = "popov.art@hotmail.com"
-    session.add(user)
+    job = Jobs()
+    job.team_leader = 1
+    job.job = "deployment of residential modules 1 and 2"
+    job.work_size = 15
+    job.collaborators = "2, 3"
+    job.start_date = datetime.now()
+    job.is_finished = False
+    session.add(job)
 
     session.commit()
 
